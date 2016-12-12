2cae8973737839a2182d2f3bb7788c66cd0b5299
Deathande <dockconnellyoneal@gmail.com>
2016-12-12 02:35:53 -0500

    Almost have it working without threading

--- knapalg.cpp
+++ knapalg.cpp
@@ -1,13 +1,8 @@
 #include "knapalg.h"
 
-queue<d_type*> t_disk_queue;
-queue<d_type*> d_disk_queue;
-pthread_t disk_io_thread;
-sem_t sem_full;
-sem_t sem_empty;
-pthread_mutex_t mux;
+#define OUT_FILE "temp_data.out"
+
 FILE* disk;
-bool cont = true;
 int mw;
 int ni;
 int bs;
@@ -18,34 +13,20 @@ void buffered_table(d_type* weights,
   #ifdef DEBUG
     cout << "Table excluding zero rows" << endl;
   #endif 
-  d_type* current = new d_type[mw+1];
-  d_type* prev = new d_type[mw+1];
-  d_type* c_dir = new d_type[mw+1];
+  int* current = new int[mw+1];
+  int* prev = new int[mw+1];
+  int* c_dir= new int[mw+1];
+  disk = fopen(OUT_FILE, "w");
 
-  sem_init(&sem_empty, 0, 0);
-  sem_init(&sem_full, 0, bs);
-  pthread_mutex_init(&mux, NULL);
   for (int i = 0; i <= mw; i++)
   {
     current[i] = 0;
     c_dir[i] = 0;
   }
-  sem_wait(&sem_full);
-  t_disk_queue.push(current);
-  d_disk_queue.push(c_dir);
-  sem_post(&sem_empty);
-  prev = current;
-
-  pthread_create(&disk_io_thread, 
-                 NULL, 
-        	 write_to_disk,
-        	 NULL);
-
+  *prev = *current;
   for (int i = 1; i <= ni; i++)
   {
-    current = new d_type[mw+1];
-    c_dir = new d_type[mw+1];
-    current[0] = 0;
+    c_dir[0] = i-1;
     for (int j = 1; j <= mw; j++)
     {
       if (weights[i] > (d_type)j)
@@ -53,7 +34,7 @@ void buffered_table(d_type* weights,
         current[j] = prev[j];
         c_dir[j] = j;
         #ifdef DEBUG
-          cout << current[j] << " ";
+          //cout << c_dir[j] << " ";
         #endif
       }
       else
@@ -66,7 +47,7 @@ void buffered_table(d_type* weights,
           current[j] = prev[j];
           c_dir[j] = j;
           # ifdef DEBUG
-            cout << current[j] << " ";
+            //cout << c_dir[j] << " ";
           # endif
         }
         else
@@ -74,73 +55,28 @@ void buffered_table(d_type* weights,
           current[j] = n_above;
           c_dir[j] = n_above_index;
           # ifdef DEBUG
-            cout << current[j] << " ";
+            //cout << c_dir[j] << " ";
           # endif
         }
       }
     }
-    sem_wait(&sem_full);
-    pthread_mutex_lock(&mux);
-    t_disk_queue.push(current);
-    d_disk_queue.push(c_dir);
-    pthread_mutex_unlock(&mux);
-    sem_post(&sem_empty);
-    //temp = prev;
-    //prev = current;
-    //current = temp;
+    for (int i = 0; i < mw+1; i++)
+      cout << c_dir[i] << " ";
+    cout << endl;
+    fwrite(c_dir,
+           sizeof(int),
+           mw+1,
+           disk);
+    *prev = *current;
+
     #ifdef DEBUG
       cout << endl;
     #endif
   }
-  cont = false;
-  pthread_join(disk_io_thread, NULL);
-  //delete [] current;
-  //delete [] c_dir;
-  sem_destroy(&sem_full);
-  sem_destroy(&sem_empty);
-  pthread_mutex_destroy(&mux);
-  //delete [] prev;
-}
-
-void* write_to_disk(void* non_used)
-{
-  disk = fopen("temp_file.dat", "w");
-  d_type* row;
-  while (cont || t_disk_queue.size() != 0)
-  {
-    sem_wait(&sem_empty);
-    pthread_mutex_lock(&mux);
-    row = t_disk_queue.front();
-    fwrite(row, sizeof(row[0]), mw+1, disk);
-    row = d_disk_queue.front();
-    fwrite(row, sizeof(row[0]), mw+1, disk);
-    t_disk_queue.pop();
-    pthread_mutex_unlock(&mux);
-    d_disk_queue.pop();
-    sem_post(&sem_full);
-  }
+  delete [] current;
+  delete [] prev;
+  delete [] c_dir;
   fclose(disk);
-  return NULL;
-}
-
-void* read_from_disk(void* not_used)
-{
-  d_type* t_row = new d_type[mw+1];
-  d_type* d_row = new d_type[mw+1];
-  disk = fopen("temp_file.dat", "r");
-  while (cont)
-  {
-    sem_wait(&sem_full); 
-    fread(t_row, sizeof(t_row[0]), mw+1, disk);
-    fread(d_row, sizeof(d_row[0]), mw+1, disk);
-    pthread_mutex_lock(&mux);
-    t_disk_queue.push(t_row);
-    d_disk_queue.push(d_row);
-    pthread_mutex_unlock(&mux);
-    sem_post(&sem_empty);
-  }
-  fclose(disk);
-  return NULL;
 }
 
 vector<int> get_items( d_type* weights,
@@ -149,50 +85,36 @@ vector<int> get_items( d_type* weights,
         	       int num_items,
         	       int buffer_size)
 {
-  int i = num_items;
-  int j = max_weight;
+  int last;
   mw = max_weight;
   ni = num_items;
   bs = buffer_size;
+  int i = ni;
+  int j = mw;
 
   buffered_table(weights, values);
-  cont = true;
-  vector<int> indicies;
-  pthread_create(&disk_io_thread, 
-                 NULL, 
-        	 read_from_disk,
-        	 NULL);
-  sem_init(&sem_empty, 0, 0);
-  sem_init(&sem_full, 0, bs);
-  pthread_mutex_init(&mux, NULL);
-
-  sem_wait(&sem_empty);
-  pthread_mutex_lock(&mux);
-  d_type* c_table = t_disk_queue.front();
-  d_type* c_dir = d_disk_queue.front();
-  t_disk_queue.pop();
-  d_disk_queue.pop();
-  pthread_mutex_unlock(&mux);
-  sem_post(&sem_full);
-  while (c_table[j] != 0)
+  vector<int> indices;
+  disk = fopen(OUT_FILE, "r"); 
+  fseek(disk,
+        -i * mw * sizeof(int) - (j+1) * sizeof(int),
+	SEEK_END);
+  last = j;
+  fread(&j, sizeof(int), 1, disk);
+  cout << "br: " << j << endl;
+  i--;
+  for (int x = 0; x < ni; x++)
   {
-    if (c_dir[j] < (d_type)j)
-      indicies.push_back(i);
-    j = c_dir[j];
-    sem_wait(&sem_empty);
-    pthread_mutex_lock(&mux);
-    c_dir = d_disk_queue.front();
-    d_disk_queue.pop();
-    c_table = t_disk_queue.front();
-    t_disk_queue.pop();
-    pthread_mutex_unlock(&mux);
-    sem_post(&sem_full);
+    if (last < j)
+      indices.push_back(i);
+    fseek(disk,
+          -i * (mw+1) * sizeof(int) - (j+1) * sizeof(int),
+          SEEK_END);
+    last = j;
+    fread(&j, sizeof(int), 1, disk);
     i--;
   }
-  sem_destroy(&sem_full);
-  sem_destroy(&sem_empty);
-  pthread_mutex_destroy(&mux);
-  return indicies;
+
+  return indices;
 }
 
 
