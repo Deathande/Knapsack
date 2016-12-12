89e553c70aa0321e5d5d0a48cdb23f525e693504
Deathande <dockconnellyoneal@gmail.com>
2016-12-12 04:52:37 -0500

    Removing unecessary code

--- knapalg.cpp
+++ knapalg.cpp
@@ -75,9 +75,7 @@ void buffered_table(d_type* weights,
       cout << endl;
     #endif
   }
-  cout << current[mw] << endl;
   delete [] current;
-  //delete [] prev;
   delete [] c_dir;
   fclose(disk);
 }
@@ -106,67 +104,11 @@ vector<int> get_items( d_type* weights,
 	SEEK_SET);
     fread(&j, sizeof(int), 1, disk);
     if (last > j)
+    {
       indices.push_back(i);
+    }
     i--;
   }
 
   return indices;
-}
-
-
-
-// Build a interconnected linked list where the tail
-// pointer points to the head pointer and vis versa.
-void init_buff()
-{
-  /*
-  table = new dock_dstructure::node<d_type*>();
-  directions = new dock_dstructure::node<d_type*>();
-  dock_dstructure::node<d_type*>* t_cn = table;
-  dock_dstructure::node<d_type*>* d_cn = directions;
-  table->set_data(new d_type[mw+1]);
-  directions->set_data(new d_type[mw+1]);
-
-  for (int i = 0; i < bs; i++)
-  {
-    dock_dstructure::node<d_type*>* t_new_node;
-    dock_dstructure::node<d_type*>* d_new_node;
-
-    t_new_node = new dock_dstructure::node<d_type*>();
-    d_new_node = new dock_dstructure::node<d_type*>();
-
-    t_new_node->set_data(new d_type[mw+1]);
-    d_new_node->set_data(new d_type[mw+1]);
-
-    t_cn->set_n_node(t_new_node);
-    d_cn->set_n_node(d_new_node);
-
-    t_new_node->set_p_node(t_cn);
-    d_new_node->set_p_node(d_cn);
-
-    t_cn = t_new_node;
-    d_cn = d_new_node;
-  }
-  t_cn->set_n_node(table);
-  table->set_p_node(t_cn);
-  d_cn->set_n_node(directions);
-  directions->set_p_node(d_cn);
-  */
-}
-
-
-void start_table()
-{
-  /*
-  // initialize the first row
-  for (int i = 1; i <= mw; i++)
-    table->get_data()[i] = 0;
-  // Initialize the first column
-  dock_dstructure::node<d_type*>* cn = table;
-  do
-  {
-    cn->get_data()[0] = 0;
-    cn = cn->n_node();
-  } while (cn != table);
-  */
 }
